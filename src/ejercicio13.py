077a7bbd2ba2500e1aad10072aa61e82229030bc
javierac11 <javieracedocastrillon@gmail.com>
2023-10-19 16:20:15 +0200

    Ejercicio 13

---
+++ src/ejercicio13.py
@@ -0,0 +1,11 @@
+#Ejercicio 13¶
+#Escribir un programa que muestre el eco de todo lo que el usuario introduzca hasta que el usuario
+#escriba “salir” que terminará.
+
+def repetir():
+    entrada = input(f"Introduzca lo que sea, si introduces \"salir\" terminara: ")
+    while entrada != "salir":
+        print(entrada)
+        entrada = input(f"Introduzca lo que sea, si introduces \"salir\" terminara: ")
+        
+repetir()
