9b3e286845c7b90a43903658792c6cc58f0d26da
javierac11 <javieracedocastrillon@gmail.com>
2023-10-19 16:14:31 +0200

    Ejercicio 12 + test

--- src/ejercicio12.py
+++ src/ejercicio12.py
@@ -2,5 +2,26 @@
 #Escribir un programa en el que se pregunte al usuario por una frase y una letra, 
 # y muestre por pantalla el número de veces que aparece la letra en la frase.
 
-def contarPalabras(palabra, letra):
+def leeLetra():
+    letra = input("Introduce una letra: ")
+    while len(letra) != 1:
+        letra = input("ERROR.Introduce una letra: ")
+    return letra
+
+def leePalabra(mensaje: str):
+    palabra = input(mensaje)
+    return palabra
+
+def contarLetras(frase, letra):
+    cont = 0
+    for x in frase:
+        if str.upper(x) == str.upper(letra):
+            cont += 1 
+    return cont
+
+if __name__ == "__main__":
     
+    palabra = leePalabra("Introduce una palabra: ")
+    letra = leeLetra()
+    
+    print(contarLetras(palabra, letra))
